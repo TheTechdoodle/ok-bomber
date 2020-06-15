7dde3c180315f866f3cd8d6da0b573e75db7cfe8
TheTechdoodle <admin+github@darkender.com>
2020-06-14 21:44:34 -0700

    add particles to incendiary addon

--- src/main/java/com/darkender/plugins/okbomber/custom/addons/IncendiaryAddon.java
+++ src/main/java/com/darkender/plugins/okbomber/custom/addons/IncendiaryAddon.java
@@ -2,7 +2,9 @@ package com.darkender.plugins.okbomber.custom.addons;
 
 import com.darkender.plugins.okbomber.TNTData;
 import com.darkender.plugins.okbomber.custom.TNTAddon;
+import org.bukkit.Particle;
 import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class IncendiaryAddon extends TNTAddon
 {
@@ -33,5 +35,20 @@ public class IncendiaryAddon extends TNTAddon
     public void onIgnite(TNTPrimed tnt, TNTData data)
     {
         tnt.setIsIncendiary(true);
+    }
+    
+    @Override
+    public void entityTick(TNTPrimed tnt, TNTData data)
+    {
+        tnt.getWorld().spawnParticle(Particle.FLAME, tnt.getLocation().add(0.0, 1.1, 0.0), 0);
+    }
+    
+    @Override
+    public void onExplode(EntityExplodeEvent event, TNTData data)
+    {
+        if(!event.isCancelled())
+        {
+            event.getLocation().getWorld().spawnParticle(Particle.FLAME, event.getLocation(), 250);
+        }
     }
 }
